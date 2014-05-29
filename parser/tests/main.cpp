d7ff8245151241bae2ea21ef51c9c14a6b1b1349
Sergey Vidyuk <sir.vestnik@gmail.com>
2014-05-29 22:34:35 +0700

    Задание имени пакета

--- parser/tests/main.cpp
+++ parser/tests/main.cpp
@@ -3,31 +3,37 @@
 #include "parser/metaparser.h"
 
 TEST(Parser, zeroParamFunc) {
-    const char *input = "foo() {return 5;}";
+    const char *input = "package test; foo() {return 5;}";
     meta::Parser parser;
     ASSERT_NO_THROW(parser.parse(input));
 }
 
 TEST(Parser, oneParamFunc) {
-    const char *input = "foo(x) {return 5*x*x - 2*x + 3;}";
+    const char *input = "package test; foo(x) {return 5*x*x - 2*x + 3;}";
     meta::Parser parser;
     ASSERT_NO_THROW(parser.parse(input));
 }
 
 TEST(Parser, twoParamFunc) {
-    const char *input = "foo(x, y) {return 5*x + 6/y;}";
+    const char *input = "package test; foo(x, y) {return 5*x + 6/y;}";
     meta::Parser parser;
     ASSERT_NO_THROW(parser.parse(input));
 }
 
 TEST(Parser, twoFunc) {
-    const char *input = "foo(x) {return 5*x;}\nbar(x) {return x/5;}";
+    const char *input = "package test; foo(x) {return 5*x;}\nbar(x) {return x/5;}";
     meta::Parser parser;
     ASSERT_NO_THROW(parser.parse(input));
 }
 
 TEST(Parser, funcCall) {
-    const char *input = "foo(x) {return 5*x;}\nbar(y) {return 5*foo(y/5);}";
+    const char *input = "package test; foo(x) {return 5*x;}\nbar(y) {return 5*foo(y/5);}";
+    meta::Parser parser;
+    ASSERT_NO_THROW(parser.parse(input));
+}
+
+TEST(Parser, emptyPackage) {
+    const char *input = "package test.test;";
     meta::Parser parser;
     ASSERT_NO_THROW(parser.parse(input));
 }
