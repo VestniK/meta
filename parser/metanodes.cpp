a09c9d6ca1ce3f60177605bea28bddf2b93602f3
Sergey Vidyuk <sir.vestnik@gmail.com>
2014-06-19 00:19:27 +0700

    Элементы дерева для операций констант и переменных

--- parser/metanodes.cpp
+++ parser/metanodes.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <cstdlib>
 
 #include "metanodes.h"
 
@@ -9,6 +10,31 @@ Function::Function(const meta::StackFrame* start, size_t size): Node(start, size
     assert(size > 1);
     static const size_t namePos = 0;
     mName.assign(start[namePos].start, start[namePos].end - start[namePos].start);
+}
+
+BinaryOp::BinaryOp(const StackFrame* start, size_t size): Node(start, size)
+{
+    assert(size == 3);
+    static const size_t opPos = 1;
+    switch (*start[opPos].start) {
+        case '+': mOp = add; break;
+        case '-': mOp = sub; break;
+        case '*': mOp = mul; break;
+        case '/': mOp = div; break;
+    }
+}
+
+Number::Number(const StackFrame* start, size_t size): Node(start, size)
+{
+    assert(size == 1);
+    std::string strVal(start->start, start->end - start->start);
+    mValue = atoi(strVal.c_str());
+}
+
+Var::Var(const StackFrame* start, size_t size): Node(start, size)
+{
+    assert(size == 1);
+    mName.assign(start->start, start->end - start->start);
 }
 
 } // namespace meta
--- parser/metanodes.h
+++ parser/metanodes.h
@@ -18,6 +18,42 @@ private:
     std::string mName;
 };
 
+class BinaryOp: public Node
+{
+meta_NODE
+public:
+    BinaryOp(const StackFrame *start, size_t size);
+
+    enum Operation {add, sub, mul, div};
+    Operation operation() const {return mOp;}
+
+private:
+    Operation mOp;
+};
+
+class Number: public Node
+{
+meta_NODE
+public:
+    Number(const StackFrame *start, size_t size);
+
+    int value() const {return mValue;}
+
+private:
+    int mValue;
+};
+
+class Var: public Node
+{
+meta_NODE
+public:
+    Var(const StackFrame *start, size_t size);
+
+    const std::string &name() const {return mName;}
+private:
+    std::string mName;
+};
+
 } // namespace meta
 
 #endif // META_NODES_H
