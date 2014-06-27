6acc7c01868c2c349e1e2747a0641ace2591258c
Sergey Vidyuk <sir.vestnik@gmail.com>
2014-06-28 02:13:18 +0700

    Тесты синтаксического разбора арифметических операций
    Проверяю, что приоритеты у сложения и умножения правильные, так же проверяю
    что скобки группируют операции правильно.

---
+++ parser/tests/arythmetic.cpp
@@ -0,0 +1,90 @@
+#include <cassert>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "parser/package.h"
+
+struct Operation
+{
+    meta::BinaryOp::Operation operation;
+    int left, right;
+};
+
+class LoggingCalc: public meta::Visitor
+{
+public:
+    virtual void leave(meta::Number *num)
+    {
+        mCalcStack.push_back(num->value());
+    }
+
+    virtual void leave(meta::BinaryOp *op)
+    {
+        assert(mCalcStack.size() >= 2);
+        mCalcSequence.push_back(Operation());
+        Operation &current = mCalcSequence.back();
+        current.operation = op->operation();
+        current.right = mCalcStack.back();
+        mCalcStack.pop_back();
+        current.left = mCalcStack.back();
+        switch (current.operation) {
+            case meta::BinaryOp::add: mCalcStack.back() = current.left + current.right; break;
+            case meta::BinaryOp::sub: mCalcStack.back() = current.left - current.right; break;
+            case meta::BinaryOp::mul: mCalcStack.back() = current.left*current.right; break;
+            case meta::BinaryOp::div: mCalcStack.back() = current.left/current.right; break;
+            default: assert(false);
+        }
+    }
+
+    const std::vector<Operation> &calcSequence() const {return mCalcSequence;}
+    int result() const
+    {
+        assert(mCalcStack.size() == 1);
+        return mCalcStack.back();
+    }
+
+private:
+    std::vector<int> mCalcStack;
+    std::vector<Operation> mCalcSequence;
+};
+
+TEST(Arythmetic, priorities)
+{
+    const char *content = "package test; foo() {return 5+7*8;}";
+    Package pkg;
+    pkg.parse("test.meta", content);
+    LoggingCalc calc;
+    pkg.functions["foo"]->walk(&calc);
+    ASSERT_EQ(calc.calcSequence().size(), 2);
+
+    ASSERT_EQ(calc.calcSequence()[0].operation, meta::BinaryOp::mul);
+    ASSERT_EQ(calc.calcSequence()[0].left, 7);
+    ASSERT_EQ(calc.calcSequence()[0].right, 8);
+
+    ASSERT_EQ(calc.calcSequence()[1].operation, meta::BinaryOp::add);
+    ASSERT_EQ(calc.calcSequence()[1].left, 5);
+    ASSERT_EQ(calc.calcSequence()[1].right, 56);
+
+    ASSERT_EQ(calc.result(), 61);
+}
+
+TEST(Arythmetic, parenthesis)
+{
+    const char *content = "package test; foo() {return (11+5)/8;}";
+    Package pkg;
+    pkg.parse("test.meta", content);
+    LoggingCalc calc;
+    pkg.functions["foo"]->walk(&calc);
+    ASSERT_EQ(calc.calcSequence().size(), 2);
+
+    ASSERT_EQ(calc.calcSequence()[0].operation, meta::BinaryOp::add);
+    ASSERT_EQ(calc.calcSequence()[0].left, 11);
+    ASSERT_EQ(calc.calcSequence()[0].right, 5);
+
+    ASSERT_EQ(calc.calcSequence()[1].operation, meta::BinaryOp::div);
+    ASSERT_EQ(calc.calcSequence()[1].left, 16);
+    ASSERT_EQ(calc.calcSequence()[1].right, 8);
+
+    ASSERT_EQ(calc.result(), 2);
+}
