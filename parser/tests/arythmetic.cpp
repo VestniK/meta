56a8306fb232bb4b8640a97b49b8ed1d77d73759
Sergey Vidyuk <sir.vestnik@gmail.com>
2014-09-25 01:11:11 +0700

    Правильные приоритеты и тесты их правильности

--- parser/tests/arythmetic.cpp
+++ parser/tests/arythmetic.cpp
@@ -79,31 +79,6 @@ private:
     std::vector<Operation> mCalcSequence;
 };
 
-TEST(Arythmetic, priorities)
-{
-    const char *input = "package test; int foo() {return 5*2+7*8;}";
-    meta::Parser parser;
-    std::unique_ptr<meta::AST> ast;
-    ASSERT_NO_THROW(ast = std::unique_ptr<meta::AST>(parser.parse(input)));
-    LoggingCalc calc;
-    ast->walk(&calc);
-    ASSERT_EQ(calc.calcSequence().size(), 3);
-
-    ASSERT_EQ(calc.calcSequence()[0].operation, meta::BinaryOp::mul);
-    ASSERT_EQ(calc.calcSequence()[0].left, 5);
-    ASSERT_EQ(calc.calcSequence()[0].right, 2);
-
-    ASSERT_EQ(calc.calcSequence()[1].operation, meta::BinaryOp::mul);
-    ASSERT_EQ(calc.calcSequence()[1].left, 7);
-    ASSERT_EQ(calc.calcSequence()[1].right, 8);
-
-    ASSERT_EQ(calc.calcSequence()[2].operation, meta::BinaryOp::add);
-    ASSERT_EQ(calc.calcSequence()[2].left, 10);
-    ASSERT_EQ(calc.calcSequence()[2].right, 56);
-
-    ASSERT_EQ(calc.result(), 66);
-}
-
 TEST(Arythmetic, parenthesis)
 {
     const char *input = "package test; int foo() {return 2*(11+5)/8;}";
---
+++ parser/tests/priority.cpp
@@ -0,0 +1,89 @@
+/*
+ * Meta language compiler
+ * Copyright (C) 2014  Sergey Vidyuk <sir.vestnik@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <cassert>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "parser/binaryop.h"
+#include "parser/parse.h"
+
+namespace {
+
+struct TestData
+{
+    const char *expr;
+    std::vector<meta::BinaryOp::Operation> opSequence;
+};
+
+class Priority: public ::testing::TestWithParam<TestData>
+{
+public:
+};
+
+}
+
+TEST_P(Priority, priority)
+{
+    const auto &param = GetParam();
+    const char *tmpl = "package test; int foo() {return %s;}";
+    char input[sizeof(tmpl) + strlen(param.expr)];
+    sprintf(input, tmpl, param.expr);
+    std::unique_ptr<meta::AST> ast;
+    ASSERT_NO_THROW(ast = std::unique_ptr<meta::AST>(parse(input, strlen(input))));
+    std::vector<meta::BinaryOp::Operation> opSequence;
+    ast->walkBottomUp<meta::BinaryOp>([&](meta::BinaryOp *node) {opSequence.push_back(node->operation());});
+    ASSERT_EQ(opSequence, param.opSequence);
+}
+
+INSTANTIATE_TEST_CASE_P(Operations, Priority, ::testing::Values(
+    TestData({"1+2-3+4", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::add, meta::BinaryOp::sub, meta::BinaryOp::add})}),
+    TestData({"1-2+3-4", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::sub, meta::BinaryOp::add, meta::BinaryOp::sub})}),
+
+    TestData({"1+2*3+4", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::mul, meta::BinaryOp::add, meta::BinaryOp::add})}),
+    TestData({"1*2+3*4", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::mul, meta::BinaryOp::mul, meta::BinaryOp::add})}),
+
+    TestData({"1+2/3+4", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::div, meta::BinaryOp::add, meta::BinaryOp::add})}),
+    TestData({"1/2+3/4", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::div, meta::BinaryOp::div, meta::BinaryOp::add})}),
+
+    TestData({"1-2/3-4", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::div, meta::BinaryOp::sub, meta::BinaryOp::sub})}),
+    TestData({"1/2-3/4", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::div, meta::BinaryOp::div, meta::BinaryOp::sub})}),
+
+    TestData({"1-2*3-4", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::mul, meta::BinaryOp::sub, meta::BinaryOp::sub})}),
+    TestData({"1*2-3*4", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::mul, meta::BinaryOp::mul, meta::BinaryOp::sub})}),
+
+    TestData({"1*2/3*4", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::mul, meta::BinaryOp::div, meta::BinaryOp::mul})}),
+    TestData({"1/2*3/4", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::div, meta::BinaryOp::mul, meta::BinaryOp::div})}),
+
+    TestData({"1+2*3>3-4/5", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::mul, meta::BinaryOp::add, meta::BinaryOp::div, meta::BinaryOp::sub, meta::BinaryOp::greater})}),
+    TestData({"1+2*3>=3-4/5", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::mul, meta::BinaryOp::add, meta::BinaryOp::div, meta::BinaryOp::sub, meta::BinaryOp::greatereq})}),
+    TestData({"1+2*3<3-4/5", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::mul, meta::BinaryOp::add, meta::BinaryOp::div, meta::BinaryOp::sub, meta::BinaryOp::less})}),
+    TestData({"1+2*3<=3-4/5", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::mul, meta::BinaryOp::add, meta::BinaryOp::div, meta::BinaryOp::sub, meta::BinaryOp::lesseq})}),
+    TestData({"1+2*3==3-4/5", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::mul, meta::BinaryOp::add, meta::BinaryOp::div, meta::BinaryOp::sub, meta::BinaryOp::equal})}),
+    TestData({"1+2*3!=3-4/5", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::mul, meta::BinaryOp::add, meta::BinaryOp::div, meta::BinaryOp::sub, meta::BinaryOp::noteq})}),
+
+    TestData({"1<2 == 3<=5", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::less, meta::BinaryOp::lesseq, meta::BinaryOp::equal})}),
+    TestData({"1>2 == 3>=5", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::greater, meta::BinaryOp::greatereq, meta::BinaryOp::equal})}),
+    TestData({"1<2 != 3<=5", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::less, meta::BinaryOp::lesseq, meta::BinaryOp::noteq})}),
+    TestData({"1>2 != 3>=5", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::greater, meta::BinaryOp::greatereq, meta::BinaryOp::noteq})}),
+
+    TestData({"1 == 2 != 3 == 4", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::equal, meta::BinaryOp::noteq, meta::BinaryOp::equal})}),
+    TestData({"1 != 2 == 3 != 4", std::vector<meta::BinaryOp::Operation>({meta::BinaryOp::noteq, meta::BinaryOp::equal, meta::BinaryOp::noteq})})
+));
