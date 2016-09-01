188eb2c5c6e889df2ee6bec3f970d6389c5665ab
Sergey Vidyuk <sir.vestnik@gmail.com>
2016-09-02 00:04:52 +0700

    Эксперименты с ленивой трансформацией последовательностей

--- utils/range.h
+++ utils/range.h
@@ -23,6 +23,74 @@
 
 namespace meta::utils {
 
+using std::begin;
+using std::end;
+
+template<typename T>
+concept bool Range = requires(T t) {
+    {begin(t)};
+    {end(t)};
+    {++begin(t)};
+    {begin(t) != end(t)} -> bool;
+    {*begin(t)};
+};
+
+template<Range R>
+struct range_traits {
+    using iterator = std::decay_t<decltype(begin(std::declval<R>()))>;
+    using value_type = std::decay_t<decltype(*std::declval<iterator>())>;
+};
+
+template<Range R>
+using range_iterator_t = typename range_traits<R>::iterator;
+template<Range R>
+using range_value_t = typename range_traits<R>::value_type;
+
+template<typename I>
+concept bool Iterator = requires(I i) {
+    {*i};
+    {++i};
+};
+
+template<typename I, typename F>
+concept bool IteratorValueTransformation =
+    Iterator<I> &&
+    requires(I i, F f)
+{
+    {f(*i)};
+};
+
+template<typename Iter, typename Func>
+    requires IteratorValueTransformation<Iter, Func>
+struct TransformIter {
+    Func func;
+    Iter it;
+
+    bool operator!= (Iter end) {return it != end;}
+    auto operator* () {return func(*it);}
+    TransformIter& operator++ () {++it; return *this;}
+};
+
+template<typename Iter, typename Sentinel>
+struct View {
+    Iter begin_it;
+    Sentinel end_it;
+
+    Iter begin() const {return begin_it;}
+    Sentinel end() const {return end_it;}
+    bool empty() const {return !(begin_it != end_it);}
+};
+
+template<typename R, typename F>
+    requires Range<R> && IteratorValueTransformation<range_iterator_t<R>, F>
+auto operator| (R& rng, F&& func) {
+    using Iter = decltype(begin(rng));
+    return View<TransformIter<Iter, F>, Iter>{
+        TransformIter<Iter, F>{std::forward<F>(func), begin(rng)},
+        end(rng)
+    };
+}
+
 template<typename Iter>
 struct Slice {
     Iter begin_it;
@@ -62,6 +130,9 @@ auto adjacent_find(const Container& cnt, BinPred&& pred) {
     return std::adjacent_find(cnt.begin(), cnt.end(), std::forward<BinPred>(pred));
 }
 
+/**
+ * Итератор для обхода коллекции по парам соседних элементов.
+ */
 template<typename Iter>
 struct SegmentsIter {
     Iter head;
--- utils/tests/range.cpp
+++ utils/tests/range.cpp
@@ -32,5 +32,16 @@ INSTANTIATE_TEST_CASE_P(SegmentsRange, Ranges, ::testing::Values(
     TestData{{1, 2, 3, 4, 5}, {{1,2}, {2,3}, {3,4}, {4,5}}}
 ));
 
+TEST(Ranges, transform) {
+    std::vector<int> arr = {1, 3, 6, 7, 3, 4, 15};
+    size_t pos = 0;
+    for (int square: arr | [](int x) {return x*x;}) {
+        ASSERT_LT(pos, std::size(arr));
+        EXPECT_EQ(square, arr[pos]*arr[pos]);
+        ++pos;
+    }
+    EXPECT_EQ(pos, std::size(arr));
+}
+
 }
 } // namespace meta::utils
