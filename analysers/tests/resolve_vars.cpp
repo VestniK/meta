c50c8609b1709b3f60a6ca78480f4956513171b3
Sergey Vidyuk <sir.vestnik@gmail.com>
2016-09-27 23:01:56 +0700

    Тест связывания переменных с декларациями выявляющий проблему

---
+++ analysers/tests/resolve_vars.cpp
@@ -0,0 +1,43 @@
+
+#include <gtest/gtest.h>
+
+#include "utils/testtools.h"
+
+#include "parser/metaparser.h"
+#include "parser/var.h"
+#include "parser/vardecl.h"
+
+#include "analysers/actions.h"
+#include "analysers/resolver.h"
+#include "analysers/semanticerror.h"
+
+namespace meta::analysers {
+namespace {
+
+TEST(ResolveVars, simple) {
+    utils::string_view input = R"META(
+        package test;
+
+        extern int dist(int y);
+
+        int foo(int x) {
+            return dist(x+5);
+        }
+    )META";
+
+    Parser parser;
+    Actions act;
+    parser.setNodeActions(&act);
+    parser.setParseActions(&act);
+    ASSERT_PARSE(parser, "test.meta", input);
+    auto ast = parser.ast();
+    ASSERT_ANALYSE(v2::resolve(ast, act.dictionary()));
+    auto vars = ast->getChildren<Var>(infinitDepth);
+    ASSERT_EQ(vars.size(), 1u);
+    ASSERT_NE(vars[0]->declaration(), nullptr);
+    EXPECT_TRUE(vars[0]->declaration()->flags() & VarFlags::argument);
+    EXPECT_EQ(vars[0]->declaration()->name(), "x");
+}
+
+} // anonymous namespace
+} // namespace meta::analysers
