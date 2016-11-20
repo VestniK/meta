044b36308a1322246f3f972e1c0f5ba58ab8f595
Sergey Vidyuk <sir.vestnik@gmail.com>
2016-11-20 22:29:20 +0700

    Убил ненужный хедер

--- typesystem/typesstore.cpp
+++ typesystem/typesstore.cpp
@@ -16,9 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-
-#include <experimental/string_view>
-
 #include "utils/contract.h"
 #include "utils/exception.h"
 
